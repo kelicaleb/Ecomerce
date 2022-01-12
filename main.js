d895de378211c38202c17d84b0566c59d27152bd
kelicaleb <kelicaleb@gmail.com>
2022-01-12 04:19:30 +0300

    ip3

--- main.js
+++ main.js
@@ -1,1 +1,35 @@
-alert("welcome home")
+$(document).ready(function(){
+    $("#para1").hide();
+    $("#para2").hide();
+    $("#design").click(function(){
+        $("#para1").toggle();
+        $("#para2").toggle();
+    });
+    $("#dev").hide();
+    $("#development").click(function(){
+        $("#dev").toggle();
+
+
+    });
+    $("#prod2").hide();
+    $("#product").click(function(){
+        $("#prod2").toggle();
+    });
+
+
+
+});
+const form = document.querySelector("#content");
+form.addEventListener("submit", function(button){
+    button.preventDefault();
+    const jsform = new FormData(form)
+    const message = jsform.get("username")
+    if(jsform.get("username") && jsform.get("email") && jsform.get("message")){
+        alert("Thank you for contacting us " + message)
+
+    }
+    else{
+        alert("Please fill all the required fields  ")
+    }
+
+})
